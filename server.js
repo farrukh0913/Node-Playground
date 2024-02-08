0cbf0ca054a4c5046be9d83091e25623d558ae83
farrukh0913 <farrukh0913@gmail.com>
2024-02-08 06:29:43 +0500

    mongo connection

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const fs = require("fs");
 const cors = require("cors");
+const multer = require('multer');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = 3000;
@@ -20,7 +21,7 @@ app.use(express.json());
 
 app.use((req, res, next)=>{
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST,PUT,PATCH,DELETE');
   res.setHeader('Access-Control-Allow-Methods', 'Content-Type','Authorization');
   next();
 })
@@ -31,20 +32,32 @@ app.get("/", (req, res) => {
 
 // GET route - Allows to get all the items
 app.get("/api/user", (req, res) => {
-    res.status(200).json({ user: 'Waqar', id: 1 })
+    res.status(200).json({ id: 1, name: 'admin' })
 });
 
-const MongoClient = require('mongodb').MongoClient;
-const multer = require('multer');
-const mongodbConnectString = "mongodb+srv://admin:0OmQBJYlAKp8ACJy@cluster0913.aspusjs.mongodb.net/?retryWrites=true&w=majority";
-const databaseName = "Database0913";
-var database;
-
 app.listen(port, () => {
-  MongoClient.connect(mongodbConnectString, (error, client)=> {
-    database = client.db(databaseName);
-    console.log(`MongoDB connected successfully!`);
-  });
-
   console.log(`Server listening at http://localhost:${port}`);
+});
+
+mongoose.connect('mongodb+srv://admin:0OmQBJYlAKp8ACJy@cluster0913.aspusjs.mongodb.net/?retryWrites=true&w=majority');
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('MongoDB database connection established successfully');
+});
+
+app.get("/", (req, res) => {
+  res.send("<h2>API is Running!</h2>");
+});
+
+const userSchema = new mongoose.Schema({ id: Number, name: String });
+const users = mongoose.model('users', userSchema);
+app.get('/api/users', async (req, res) => {
+  try {
+    const reponseCollection = await users.find();
+    console.log('reponseCollection: ', reponseCollection);
+    res.json(reponseCollection);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server error');
+  }
 });
