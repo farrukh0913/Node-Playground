a0c47aa74833f74b1a892a581aa030234f6229f9
farrukh0913 <farrukh0913@gmail.com>
2024-02-08 07:41:37 +0500

    mongoose db connection and fetch data in node

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const multer = require('multer');
-const mongoose = require('mongoose');
+const multer = require("multer");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = 3000;
@@ -19,12 +19,16 @@ app.use(cors(corsOptions));
 // Use express.json() middleware to parse JSON bodies of requests
 app.use(express.json());
 
-app.use((req, res, next)=>{
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST,PUT,PATCH,DELETE');
-  res.setHeader('Access-Control-Allow-Methods', 'Content-Type','Authorization');
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST,PUT,PATCH,DELETE");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "Content-Type",
+    "Authorization"
+  );
   next();
-})
+});
 
 app.get("/", (req, res) => {
   res.send("<h2>API is Running!</h2>");
@@ -32,17 +36,24 @@ app.get("/", (req, res) => {
 
 // GET route - Allows to get all the items
 app.get("/api/user", (req, res) => {
-    res.status(200).json({ id: 1, name: 'admin' })
+  res.status(200).json({ id: 1, name: "admin" });
 });
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
 
-mongoose.connect('mongodb+srv://admin:0OmQBJYlAKp8ACJy@cluster0913.aspusjs.mongodb.net/?retryWrites=true&w=majority');
+const mongoConnectionString =
+  "mongodb+srv://admin:0OmQBJYlAKp8ACJy@cluster0913.aspusjs.mongodb.net/?retryWrites=true&w=majority";
+const mongoDBName = "Database0913";
+mongoose.connect(mongoConnectionString, {
+  useNewUrlParser: false,
+  dbName: mongoDBName,
+});
+
 const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log('MongoDB database connection established successfully');
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
 });
 
 app.get("/", (req, res) => {
@@ -50,14 +61,15 @@ app.get("/", (req, res) => {
 });
 
 const userSchema = new mongoose.Schema({ id: Number, name: String });
-const users = mongoose.model('users', userSchema);
-app.get('/api/users', async (req, res) => {
-  try {
-    const reponseCollection = await users.find();
-    console.log('reponseCollection: ', reponseCollection);
-    res.json(reponseCollection);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Server error');
-  }
+const userModal = mongoose.model("users", userSchema, "users");
+app.get("/api/users", async (req, res) => {
+  userModal
+    .find()
+    .then(function (users) {
+      console.log("users: ", users);
+      res.json(users);
+    })
+    .catch(function (error) {
+      console.log("error: ", error);
+    });
 });
